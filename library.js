168c578e7a1afa3b7ca83d17f362abe957c83d80
James Kane <james.kane59@outlook.com>
2022-11-01 09:17:38 +0000

    Creating function to add all books to library table in new row

---
+++ library.js
@@ -0,0 +1,66 @@
+const table = document.querySelector('.books');
+const tbody = document.querySelector('.books').getElementsByTagName('tbody')[0];
+
+let myLibrary = [];
+
+function Book(title, author, pages, published, acquired, status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.published = published;
+    this.acquired = acquired;
+    this.status = status;
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '310', '20/04/2009', '21/10/2016', 'Read');
+myLibrary.push(theHobbit);
+
+function createCheckbox() {
+    let checkbox = document.createElement('input');
+
+    checkbox.type = 'checkbox';
+    checkbox.className = 'checkboxes'; 
+
+    return checkbox;
+}
+
+function createEditFunctions() {
+    let cell = document.createElement('div')
+
+    let trash = document.createElement('img');
+    trash.setAttribute('src', 'img/delete.svg');
+    trash.classList.add('table-svgs');
+    trash.setAttribute('alt', 'Delete SVG');
+
+    let edit = document.createElement('img');
+    edit.setAttribute('src', 'img/edit.svg');
+    edit.classList.add('table-svgs');
+    edit.setAttribute('alt', 'Edit SVG');
+
+    cell.appendChild(trash);
+    cell.appendChild(edit);
+    
+    return cell;
+}
+
+function addBooksToLibrary() {
+    for (let book of myLibrary) {
+
+        let newRow = tbody.insertRow();
+        let checkbox = createCheckbox();
+       
+
+        newRow.insertCell().appendChild(checkbox);
+        newRow.insertCell().textContent = book.title;
+        newRow.insertCell().textContent = book.author;
+        newRow.insertCell().textContent = book.pages;
+        newRow.insertCell().textContent = book.published;
+        newRow.insertCell().textContent = book.acquired;
+        newRow.insertCell().textContent = book.status;
+
+        let editFunctions = createEditFunctions();
+        newRow.insertCell().appendChild(editFunctions);
+    } 
+}
+
+addBooksToLibrary();
